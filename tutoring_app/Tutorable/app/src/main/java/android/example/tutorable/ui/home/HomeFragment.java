4fdec412a77dde5f13017659db8677a5fa38162d
Matt <mzagame74@gmail.com>
2021-11-17 23:20:21 -0500

    implemented look for tutors button

--- tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/ui/home/HomeFragment.java
+++ tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/ui/home/HomeFragment.java
@@ -1,24 +1,30 @@
+/* replaceFragment code provided by:
+https://stackoverflow.com/questions/32700818/how-to-open-a-fragment-on-button-click-from-a-fragment-in-android */
 package android.example.tutorable.ui.home;
 
+import android.example.tutorable.ui.appointments.AppointmentsFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.example.tutorable.R;
 import android.example.tutorable.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private Button lookForAppointmentsButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +33,10 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        lookForAppointmentsButton =
+                (Button)root.findViewById(R.id.button_look_for_tutors);
+        lookForAppointmentsButton.setOnClickListener(this);
 
         //final TextView textView = binding.textHome;
         /*homeViewModel.getText().observe(getViewLifecycleOwner(),
@@ -37,6 +47,24 @@ public class HomeFragment extends Fragment {
             }
         });*/
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.button_look_for_tutors:
+                fragment = new AppointmentsFragment();
+                replaceFragment(fragment);
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment_activity_main, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
