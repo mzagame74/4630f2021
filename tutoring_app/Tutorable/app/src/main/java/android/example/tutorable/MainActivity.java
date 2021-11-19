a5bbcdc51987bf63ddc9162084f055650d563d33
Matt <mzagame74@gmail.com>
2021-11-19 01:30:53 -0500

    finally got constraint layouts working properly

--- tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/MainActivity.java
+++ tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/MainActivity.java
@@ -6,7 +6,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -30,7 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_appointments,
                 R.id.navigation_settings)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController;
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+        } else {
+            throw new IllegalStateException("Activity " + this + " does not " +
+                    "have a NavHostFragment");
+        }
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
