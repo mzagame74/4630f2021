edae792fd401719d12b420a7d4cbab0ace17a531
Matt <mzagame74@gmail.com>
2021-12-01 17:03:38 -0500

    changed visibility of Appointment nested class

--- tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/data/Appointments.java
+++ tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/data/Appointments.java
@@ -13,44 +13,53 @@ public class Appointments {
     private ArrayList<Appointment> appointments;
     private final int MAX_APPOINTMENTS = 10;        // 10 appointments max
 
-    private static class Appointment {
+    public static class Appointment {
+        private int hours;
         private String tutorName;
         private String location;
+        private String subject;
+        private String course;
         private Date date;
 
-        public Appointment(String tutorName, String location, Date date) {
+        public Appointment(String tutorName, String subject,
+                           String course, String location,
+                           int hours, Date date) {
             this.tutorName = tutorName;
+            this.subject = subject;
+            this.course = course;
             this.location = location;
+            this.hours = hours;
             this.date = date;
         }
 
         public String getTutorName() {
             return tutorName;
         }
-
+        public String getSubject() { return subject; }
+        public String getCourse() { return course; }
         public String getLocation() {
             return location;
         }
-
+        public int getHours() { return hours; }
         public Date getDate() {
             return date;
         }
 
-        public void setTutorName(String tutorName) {
-            this.tutorName = tutorName;
-        }
-
+        public void setTutorName(String tutorName) { this.tutorName = tutorName; }
+        public void setSubject(String subject) { this.subject = subject; }
+        public void setCourse(String course) { this.course = course; }
         public void setLocation(String location) {
             this.location = location;
         }
-
+        public void setHours(int hours) { this.hours = hours; }
         public void setDate(Date date) {
             this.date = date;
         }
 
         @NonNull
         public String toString() {
-            return tutorName + " " + location + " " + date.toString();
+            return course + " w/ " + tutorName + " at " + location + " from " +
+                    date.getHours() + ":00 to " + (date.getHours() + hours) + ":00\n";
         }
     }
 
@@ -60,18 +69,6 @@ public class Appointments {
 
     public Appointments(ArrayList<Appointment> appointments) {
         this.appointments = appointments;
-    }
-
-    public void addAppointment(Appointment appointment) {
-        appointments.add(appointment);
-    }
-
-    public void removeAppointment(Appointment appointment) {
-        appointments.remove(appointment);
-    }
-
-    public Appointment getAppointment(int index) {
-        return appointments.get(index);
     }
 
     public ArrayList<Appointment> getAppointments() {
