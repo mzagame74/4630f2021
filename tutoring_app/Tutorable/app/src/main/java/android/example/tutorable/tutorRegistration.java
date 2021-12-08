5f8574df74c3ad86a11c3cd6e8956829af642703
wilbertdelarosa <wilbert_delarosa@yahoo.com>
2021-12-07 19:46:12 -0500

    thr tutor registration

---
+++ tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/tutorRegistration.java
@@ -0,0 +1,14 @@
+package android.example.tutorable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tutorRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_registration);
+    }
+}
