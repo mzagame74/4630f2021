36ad9e018d5546fa94b9f9014c9a96fe6a85e9b5
wilbertdelarosa <wilbert_delarosa@yahoo.com>
2021-12-10 07:23:00 -0500

    Added tutor registration as a fragment

---
+++ tutoring_app/Tutorable/app/src/main/java/android/example/tutorable/ui/tutor/tutor_registration.java
@@ -0,0 +1,38 @@
+package android.example.tutorable.ui.tutor;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.example.tutorable.R;
+
+public class tutor_registration extends Fragment {
+
+    private TutorRegistrationViewModel mViewModel;
+
+    public static tutor_registration newInstance() {
+        return new tutor_registration();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tutor_registration, container, false);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TutorRegistrationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
